c1397493b4390da67837de70923693ceb10400af
kamrulhasan002 <74873549+kamrulhasan002@users.noreply.github.com>
2021-02-25 21:24:18 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+
+
+   //conter up
+   $('.counter').counterUp({
+   	delay:10,
+   	time:1000
+
+   })
+   
+
+});
+
+
+
+
+$(document).ready(function(){
+
+
+   //wow js
+ new WOW().init();
+
+});
+
+
+
+$(document).ready(function(){
+
+
+ //slick slider
+
+ $('.autoplay').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: false,
+});
+   
+
+});
+
+
+
+
+
+
+
+
